fc0e0161ae060de3d647aad9894c0a58361b3e0c
kajalm22 <mishrakajalr22@gmail.com>
2022-12-13 16:55:49 +0530

    final commit

--- config/emailConfig.js
+++ config/emailConfig.js
@@ -11,34 +11,15 @@ let transporter = nodemailer.createTransport({
     }
   })
 
-
+  // EMAIL_PASS = 'vmamvmbffhiyhubz'
 
   module.exports = transporter
 
-// let transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//         user: process.env.EMAIL_USER,
-//         pass: process.env.EMAIL_PASS
-//     }
-// })
 
 
-// const mailOptions = {
-//     from: 'mishrakajalr22@gmail.com',
-//     to: 'mishrakajalr22@gmail.com',
-//     subject: 'Portfolio',
-// }
 
-// transporter.sendMail(mailOptions, (err, result) => {
-//     if (err){
-//     console.log("error")
-//         // res.status(500).json('Oops error occurred')
-//     } else{
-//         console.log("thanks for the mail")
-//         // res.status(200).json('thanks for emailing me');
-//     }
-// })
+
+
 
 
 
